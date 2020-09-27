7168e138aed2ea1b089f5d7e751f6a4038c3c28f
RuslanA <wolfgenrifox@gmail.com>
2020-09-27 20:59:56 +0500

    two emu

--- start-default-emulator.sh
+++ start-default-emulator.sh
@@ -15,11 +15,4 @@ echo "Emulator emuf booted"
 echo "Starting emus emulator"
 $ANDROID_HOME/emulator/emulator @emus -no-window -no-audio 2>&1 &
 
-EMU_BOOTED="no"
-while [[ ${EMU_BOOTED} != *"stopped"* ]]; do
-    echo "Wating for emulator booted..."
-    sleep 5
-    EMU_BOOTED=$(adb shell getprop init.svc.bootanim || echo "no")
-done
-
 echo "Emulator emus booted"
