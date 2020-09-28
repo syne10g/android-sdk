8ee78a63b89cfbd7de7f0bc244be8153deec76cf
RuslanA <wolfgenrifox@gmail.com>
2020-09-28 08:31:15 +0500

    three emu

--- Dockerfile
+++ Dockerfile
@@ -22,6 +22,7 @@ RUN echo "y" | sdkmanager "emulator"
 RUN echo "y" | sdkmanager "platforms;android-28"
 RUN echo "y" | sdkmanager "system-images;android-28;default;x86"
 RUN echo "y" | sdkmanager --update
-RUN echo "no" | avdmanager create avd -n emuf -k "system-images;android-28;default;x86" -d 17
-RUN echo "no" | avdmanager create avd -n emus -k "system-images;android-28;default;x86" -d 17
+RUN echo "no" | avdmanager create avd -n emuone -k "system-images;android-28;default;x86" -d 17
+RUN echo "no" | avdmanager create avd -n emutwo -k "system-images;android-28;default;x86" -d 17
+RUN echo "no" | avdmanager create avd -n emuthree -k "system-images;android-28;default;x86" -d 17
 RUN rm -rf /var/lib/apt/lists/*
--- start-default-emulator.sh
+++ start-default-emulator.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 echo "Starting emuf emulator"
-$ANDROID_HOME/emulator/emulator @emuf -no-window -no-audio 2>&1 &
 
-EMU_BOOTED="no"
-while [[ ${EMU_BOOTED} != *"stopped"* ]]; do
-    echo "Wating for emulator booted..."
-    sleep 5
-    EMU_BOOTED=$(adb shell getprop init.svc.bootanim || echo "no")
+$ANDROID_HOME/emulator/emulator @emuone -no-boot-anim -no-window -no-audio 2>&1 &
+$ANDROID_HOME/emulator/emulator @emutwo -no-boot-anim -no-window -no-audio 2>&1 &
+$ANDROID_HOME/emulator/emulator @emuthree -no-boot-anim -no-window -no-audio 2>&1 &
+
+set +e
+
+bootcomplete=""
+failcounter=0
+timeout=600
+sleeptime=10
+maxfail=$((timeout / sleeptime))
+
+until [[ "${bootcomplete}" =~ "1" ]]; do
+    bootcomplete=`adb -s emulator-5556 shell getprop dev.bootcomplete 2>&1 &`
+    if [[ "${bootcomplete}" =~ "" ]]; then
+        ((failcounter += 1))
+        adb devices
+        echo "Waiting for emulator to start"
+        if [[ ${failcounter} -gt ${maxfail} ]]; then
+            echo "Timeout ($timeout seconds) reached; failed to start emulator"
+            while pkill -9 "emulator" >/dev/null 2>&1; do
+                echo "Killing emulator proces...."
+                pgrep "emulator"
+            done
+            echo "Process terminated"
+            pgrep "emulator"
+            exit 1
+        fi
+    fi
+    sleep ${sleeptime}
 done
 
-echo "Emulator emuf booted"
+adb -s emulator-5556 shell su root setprop marathon.serialno 1234567899
 
-echo "Starting emus emulator"
-$ANDROID_HOME/emulator/emulator @emus -no-window -no-audio 2>&1 &
-
-echo "Emulator emus booted"
+echo "Emulator is ready"
